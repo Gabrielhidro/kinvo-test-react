9b2b828e63417d3a333fbc8878962d14afeb665d
Gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-06 12:23:27 -0300

    Create card component

--- src/pages/fixedIncomePage/components/Main/FixedIncomeCards/index.js
+++ src/pages/fixedIncomePage/components/Main/FixedIncomeCards/index.js
@@ -1,9 +1,46 @@
-import { Container } from "./styles";
+import { Card, Container } from "./styles";
 
 export function FixedIncomeCards(){
-  return (
+let card = [
+  {
+    saldo: 'saldo',
+    valor: 1209898,
+  },
+  {
+    saldo: 'saldo2',
+    valor: 1209898,
+  },
+  {
+    saldo: 'saldo3',
+    valor: 1209898,
+  },
+  {
+    saldo: 'saldo4',
+    valor: 1209898,
+  },
+  {
+    saldo: 'saldo5',
+    valor: 1209898,
+  },
+  {
+    saldo: 'saldo6',
+    valor: 1209898,
+  }
+]
+
+
+return (
+
     <Container>
-      card
+        {card.map(item => (
+            <Card>
+              <div>
+                <p>{item.saldo}</p>
+                <h2>{item.valor}</h2>
+              </div>
+            </Card>
+          )
+        )}
     </Container>
   )
 }
--- src/pages/fixedIncomePage/components/Main/FixedIncomeCards/styles.js
+++ src/pages/fixedIncomePage/components/Main/FixedIncomeCards/styles.js
@@ -1,5 +1,35 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  display: flex;
+  gap: 10px;
+`
 
+export const Card = styled.div`
+  width: 100%;
+  padding: 14px 8px;
+
+  box-shadow: 0px 2px 20px #0000000D;
+
+  border-radius: 10px;
+  background: white;
+
+  div {
+    border-left: 2px solid var(--white-600);
+    padding-left: 8px;
+
+    p {
+      font-size: 9px;
+      font-weight: normal;
+
+      color: var(--text-400)
+    }
+
+    h2 {
+      font-size: 14px;
+      font-weight: bold;
+
+      color: var(--purple)
+    }
+  }
 `
