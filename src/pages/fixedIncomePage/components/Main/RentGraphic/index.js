923cf68cf3cd341db2d67bf5ac22d4ad77d524e8
Gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-10 09:56:50 -0300

    Insert graphic in RentGraphic

--- src/pages/fixedIncomePage/components/Main/RentGraphic/index.js
+++ src/pages/fixedIncomePage/components/Main/RentGraphic/index.js
@@ -1,9 +1,13 @@
+import High from "./HighchartsGraphic";
 import { Container } from "./styles";
 
 export function RentGraphic(){
+
+  
   return (
     <Container>
       <h1>Rentabilidade dos Títulos</h1>
+      <High />
     </Container>
   )
 }
